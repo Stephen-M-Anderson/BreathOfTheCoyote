12ae621cf110ac83815335cfd1f1d5412463a15e
Unknown <cameronmaclean17@gmail.com>
2020-05-04 09:59:34 -0700

    Level 2 Push Incomplete

---
+++ Assets/Scripts/Player/CMFreelookControlOverride.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class CMFreelookControlOverride : MonoBehaviour
+{
+    public Joystick HeyFuckYouJoystick;
+
+    public CinemachineFreeLook freeLookCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        freeLookCam.m_XAxis.Value += HeyFuckYouJoystick.Horizontal;
+        freeLookCam.m_YAxis.Value += HeyFuckYouJoystick.Vertical;
+        Debug.Log(freeLookCam.m_XAxis.Value);
+        Debug.Log(freeLookCam.m_YAxis.Value);
+
+    }
+}
