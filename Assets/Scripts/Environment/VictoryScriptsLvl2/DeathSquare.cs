1062bf154280d98d9547ae64568781b5882ccead
Cameron Maclean <cameronmaclean17@gmail.com>
2020-06-03 22:09:38 -0700

    All checks to complete game done :)

---
+++ Assets/Scripts/Environment/VictoryScriptsLvl2/DeathSquare.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathSquare : MonoBehaviour
+{
+
+    public GameObject Parent;
+    public GameObject vfx;
+    public GameObject SpawnLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Parent.transform.childCount == 0)
+        {
+            GameObject Trial2Finish = Instantiate(vfx, SpawnLocation.transform.position, Quaternion.identity);
+            Trial2Finish.name = "Trial2Finish";
+            Destroy(Parent);
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/VictoryScriptsLvl2/FinalBoss.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBoss : MonoBehaviour
+{
+    public GameObject thirdTrialBITCH;
+    public GameObject SpawnLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("Bossfight") == null)
+        {
+            GameObject Trial3Finish = Instantiate(thirdTrialBITCH, SpawnLocation.transform.position, Quaternion.identity);
+            Trial3Finish.name = "Trial3Finish";
+        }
+    }
+}
