faa2687e072b00c6a449643217944447f735790e
Stephen <brokensteelstring@gmail.com>
2020-06-03 18:44:14 -0700

    Added river teleport to the river jump puzzle

---
+++ Assets/Scripts/Environment/FallTP.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallTP : MonoBehaviour
+{
+    public GameObject destination;
+    public void Start()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        col.gameObject.transform.position = destination.transform.position;
+        Debug.Log("I'm so triggered");
+    }
+}
