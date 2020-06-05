c9a050490aacb3a5e1a37361951cd935f8ed91a7
Cameron Maclean <cameronmaclean17@gmail.com>
2020-06-04 18:10:07 -0700

    Final Push v2

--- Assets/Scripts/Enemy/TriggerSpawn.cs
+++ Assets/Scripts/Enemy/TriggerSpawn.cs
@@ -38,7 +38,8 @@ public class TriggerSpawn : MonoBehaviour
             if (GameObject.FindGameObjectWithTag("AI") == null && spawnCounter == 0)
             {
 
-                Instantiate(enemy1, enemyPosition.position, enemyPosition.rotation);
+                GameObject Enemy1 = Instantiate(enemy1, enemyPosition.position, enemyPosition.rotation);
+                Enemy1.name = "Enemy1";
                // GameObject spawnvfx = Instantiate(litAF, transform.position, transform.rotation);
                // Destroy(spawnvfx, 4.0f);
                 spawnCounter = 1;
@@ -49,26 +50,29 @@ public class TriggerSpawn : MonoBehaviour
 
     //while still in the TriggerSpawn, once it detects that the first enemy has died, it will spawn out the next enemy and increase the counter by 1
     //same will happen with the final spawn. after all enemies are dead....do something?
+    //TEMP FIX COME BACK TO AND ADD A DELAY
+    //Problem here is the checks we had previously didn't change, so second enemy would never spawn. 
+    //Initial attempt was simply changing to the gameobject.find, but it would spawn literal tons of enemies.
+    //Need to add a delay, but we can't have it spawn an infinite amount of enemies
     private void OnTriggerStay(Collider other)
     {
-
+        Debug.Log(spawnCounter + " " + permanentSleep);
         if(other.gameObject.tag == "Player")
         {
-            if(permanentSleep == true && spawnCounter == 1)
+            if(GameObject.Find("Enemy1") == null && spawnCounter == 1)
             {
-                Invoke("Spawn2", 4.0f);
-                spawnCounter = 2;
+
+                Invoke("Spawn2", 0f);
                 permanentSleep = false;
             }
 
-            else if(permanentSleep == true && spawnCounter == 2)
+            else if(GameObject.Find("Enemy2") == null && spawnCounter == 2)
             {
-                Invoke("Spawn3", 4.0f);
-                spawnCounter = 3;
+                Invoke("Spawn3", 0f);
                 permanentSleep = false;
             }
 
-            else if(permanentSleep == true && spawnCounter == 3)
+            else if(GameObject.Find("Enemy3") == null && spawnCounter == 3)
             {
                 allDead = true;
             }
@@ -78,15 +82,22 @@ public class TriggerSpawn : MonoBehaviour
     //The spawners for the enemy so they are set on delay due to invoke
     void Spawn2()
     {
-        Instantiate(enemy2, enemyPosition.position, enemyPosition.rotation);
-       // GameObject spawnvfx = Instantiate(litAF, transform.position, transform.rotation);
-      //  Destroy(spawnvfx, 4.0f);
+        GameObject Enemy2 = Instantiate(enemy2, enemyPosition.position, enemyPosition.rotation);
+        Enemy2.name = "Enemy2";
+        spawnCounter = 2;
+
+
+        // GameObject spawnvfx = Instantiate(litAF, transform.position, transform.rotation);
+        //  Destroy(spawnvfx, 4.0f);
     }
 
     void Spawn3()
     {
-        Instantiate(enemy3, enemyPosition.position, enemyPosition.rotation);
-     //   GameObject spawnvfx = Instantiate(litAF, transform.position, transform.rotation);
-      //  Destroy(spawnvfx, 4.0f);
+      GameObject Enemy3 =  Instantiate(enemy3, enemyPosition.position, enemyPosition.rotation);
+        Enemy3.name = "Enemy3";
+        spawnCounter = 3;
+
+        //   GameObject spawnvfx = Instantiate(litAF, transform.position, transform.rotation);
+        //  Destroy(spawnvfx, 4.0f);
     }
 }
