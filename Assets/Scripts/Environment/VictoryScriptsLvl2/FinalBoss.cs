a6827e4a27ffab5f7e78578aa5d58db7bf82f943
Cameron Maclean <cameronmaclean17@gmail.com>
2020-06-05 13:52:31 -0700

    Bug fix level 2 player position
    For some reason the players position was like 30 units off from where it should be. This is a temporary fix until I find out a better way to do this

--- Assets/Scripts/Environment/VictoryScriptsLvl2/FinalBoss.cs
+++ Assets/Scripts/Environment/VictoryScriptsLvl2/FinalBoss.cs
@@ -6,19 +6,21 @@ public class FinalBoss : MonoBehaviour
 {
     public GameObject thirdTrialBITCH;
     public GameObject SpawnLocation;
+    private bool alreadyspawned;
     // Start is called before the first frame update
     void Start()
     {
-        
+        alreadyspawned = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (GameObject.Find("Bossfight") == null)
+        if (GameObject.Find("Bossfight") == null && alreadyspawned == false)
         {
             GameObject Trial3Finish = Instantiate(thirdTrialBITCH, SpawnLocation.transform.position, Quaternion.identity);
             Trial3Finish.name = "Trial3Finish";
+            alreadyspawned = true;
         }
     }
 }
